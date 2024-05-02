0847b74d16556826c37545c87fb3c74f0eb02a6f
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2024-05-02 20:15:58 +0530

    added member discontinue

---
+++ src/app/api/memberdiscontinue/getmember/route.jsx
@@ -0,0 +1,47 @@
+"use server";
+
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+export async function POST(request) {
+    const prisma = new PrismaClient();
+    const body = await request.json();
+
+    try {
+
+        const member = await prisma.oRIGIN_SCHEME_USER.findUnique({
+            where: {
+                CardNo: body.cardno,
+            }
+        })
+
+        if (!member) {
+            return NextResponse.error({
+                status: 404,
+                body: { error: "Member not found" },
+            })
+        }
+
+        const scheme = await prisma.oRIGIN_SCHEME_NAME.findUnique({
+            where: {
+                SchemeCode: member?.SchemeCode,
+            }
+        });
+
+        const receipt = await prisma.oRIGIN_SCHEME_RECEIPT.findMany({
+            where: {
+                CardNo: body.cardno,
+            }
+        })
+
+        return NextResponse.json({ member, scheme, receipt });
+
+    } catch (error) {
+        return NextResponse.error({
+            status: 400,
+            body: { error: error.message },
+        });
+    } finally {
+        await prisma.$disconnect();
+    }
+}

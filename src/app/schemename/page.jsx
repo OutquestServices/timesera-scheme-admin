aa9ef9d39b0425253966e8b957fa7accaaecfc51
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2024-05-04 11:08:53 +0530

    created few pages

--- src/app/schemename/page.jsx
+++ src/app/schemename/page.jsx
@@ -1,9 +1,10 @@
+import SchemeName from '@/components/SchemeName/SchemeName'
 import React from 'react'
 
 const page = () => {
     return (
         <div>
-            
+            <SchemeName />
         </div>
     )
 }

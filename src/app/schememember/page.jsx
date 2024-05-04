31083116d4375049e21898fd2bb3bab33efea8a1
Mohith705 <mohith.21bce7511@vitapstudent.ac.in>
2024-05-04 23:32:16 +0530

    created schememember page

---
+++ src/app/schememember/page.jsx
@@ -0,0 +1,12 @@
+import SchemeMember from '@/components/SchemeMember/SchemeMember'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <SchemeMember />
+        </div>
+    )
+}
+
+export default page

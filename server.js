cd1f2b8a74a758220294708f621e87b1c65ab776
timesera <timesera9090@gmail.com>
2024-06-26 16:16:55 +0530

    updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
+const cors = require('cors'); // Import CORS middleware
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -8,8 +9,19 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+    // Use CORS middleware
+    const corsOptions = {
+      origin: '*', // Change to your client's domain if possible
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    };
+    const corsMiddleware = cors(corsOptions);
+
+    // Apply CORS middleware
+    corsMiddleware(req, res, () => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    });
   }).listen(3000, (err) => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
